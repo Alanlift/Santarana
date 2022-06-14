233b0d9ed747cb7be18a594bccd72ddc73653e05
quse <ezequiel.quse@sismogames.com>
2022-06-14 18:55:33 -0300

    eh?

--- src/scenes/preloads.js
+++ src/scenes/preloads.js
@@ -7,11 +7,21 @@ export class Preloads extends Phaser.Scene {
   }
 
   preload() {
+    this.load.image('carga_bg','public/assets/images/carga_bg.png');
+    this.load.image('menu_bg','public/assets/images/menu_bg.png');
+    this.load.image('custom_bg','public/assets/images/custom_bg.png');
+    this.load.image('tablero_bg','public/assets/images/tablero_bg.png');
+    this.load.image('ayuda_bg','public/assets/images/ayuda_bg.png');
   }
 
   create() {
-
-    // Pasa directamente a la escena del menú principal
-    this.scene.start("MainMenu");
+    // Se agrega img de fondo
+    this.add.image(this.camera.main.centerX,
+      this.camera.centerY,
+       'carga_bg');
+    // Se agrega un timer y luego a la escena del menú principal
+    setTimeout(() => {
+      this.scene.start("MainMenu");
+    }, 2000);
   }
 }
